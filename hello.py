49038971d57fff8a52485334d04d78828adf0253
aimenbashir <aimenbashir999@gmail.com>
2024-08-23 12:08:53 +0500

    first commit

---
+++ hello.py
@@ -0,0 +1,126 @@
+#01 simple print
+#print("hello world")
+#02 legal variable are 7 
+#myname ="aimen bashir"
+#print(myname)
+#name="aimen bashir"
+#print(name)
+#snake case writing style
+#_name = " fatima bash"
+#print(_name)
+#camel case writing style
+#nameFirst ="aimen"
+#print(nameFirst)
+#pascal case writing style
+#NameFirst="aimen bashir"
+#print(NameFirst)
+#name1st ="fatima"
+#print(name1st)
+#hypohented case writing style
+#name_name ="wasma"
+#print(name_name)
+#lec no: 2
+#hp=10
+#if(hp>=10):
+ #   print("healthy")
+#else:
+ #   print("unhealthy")
+ #07 input
+#name=input("enter your name= ")
+#print("hello", name)
+#08 input if condition
+#09 or operator
+#010 nested if condition
+#num1=11
+#num2=11
+#if (num1==10):
+   # if(num2==11):
+      #  print("num1 and num2 are real")
+  #  else:
+     #   print("num1 is real but num2 is not")
+#else:
+    #print("num1 is not real")
+#011 lists
+#names=[5]
+#names=
+#task
+#edu=int(input("Enter your education: "))
+#age=int(input("Enter your age: "))
+#height=int(input("Enter your height: "))
+#if(edu<14 and age<18 or height<5):
+   # print("false")
+#else: 
+    #print("true")
+#15 sets
+#set1={1,2,3,4,5,6,7,8,}
+#set1.add(9)
+#set1.remove(4)
+#print(set1,type(set1))
+#union
+#set1={1,2,3,4,5,6}
+#set2={5,6,7,8,9,10}
+#set3=set1.union(set2)
+#set3=set1|set2
+#print(set3)
+#intersection
+#set1={1,2,3,4,5,6}
+#set2={5,6,7,8,9,10}
+#set3=set1.intersection(set2)
+#set3=set1 & set2
+#print(set3)
+#diffrence
+#set3=set1.difference(set2)
+#set3= set1- set2
+#print(set3)
+#boolian
+#set1=(1,2,3,4)
+#print(1 in set1)
+#17 conditionalk ternary operator
+#x=10
+#print("pass") if x==10 else print("x is not 10")
+#while loop and for loop
+#for x in range(100):
+   #print(x)
+   #error
+#li=[1,2,3,4,5]
+#for x in li:
+#print(x)
+#i=0
+#while i<5:
+   #print(i)
+   #i+=1 or i=i+1
+#brak continue and pass
+#for i in range(5):
+#   print(i)
+#   if i==2:
+#      break
+#for i in range(5):
+#     print(i)
+#     if i == 2:
+#         pass
+# 19 nested loop
+#ls1=["orange", "banana", "apple", "cherry"]
+#for i in ls1:
+#   for j in i:
+#      "a" in i
+#  print(j)
+#name1=["j,"k","l"]
+#name2=["m","n","o"]
+
+#assignment:3
+#tup=(1,2,3,4,5)
+#s=10
+#m="m"
+#e="e"
+#y= list(tup)
+#y.insert(0,s)
+#y.append(s)
+#i=len(y)//2
+#y.insert(i,s)
+#print(y)
+
+#assignment:4
+#n= input("enter your number: ")
+#n= float(n)
+#m= n*n
+#print(m)
