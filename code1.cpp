929c95e2b6ce5b66206ab0b951dc7d0d7b1c779f
aimenbashir <aimenbashir999@gmail.com>
2024-11-20 01:43:12 +0500

    Add files via upload

---
+++ code1.cpp
@@ -0,0 +1,6 @@
+include <iostream>  // Includes the standard input-output stream library
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;  // Prints Hello, World! to the console
+    return 0;  // Indicates that the program finished successfully
+}
